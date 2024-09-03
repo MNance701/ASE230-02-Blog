d8f96bbfd6bb7135ace144fddd52ab93b666ad6c
MNance701 <nancem1@mymail.nku.edu>
2024-09-03 11:55:24 -0400

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,1 +1,6 @@
 //Page for the specific blog content from index.php
+<?php
+// Gets the post_id selected int the index.php page and returns it as $i, which is used to determine which post the user selected
+$i = $_GET["post_id"]
+
+?>
